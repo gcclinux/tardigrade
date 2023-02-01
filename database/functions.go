3156d2ef95a2909e1a4fb53cfba6ea3369d83382
Ricardo Wagemaker <java@wagemaker.co.uk>
2023-02-01 23:04:58 +0000

    Initial push

---
+++ database/functions.go
@@ -0,0 +1,33 @@
+package database
+
+func Create() {
+
+}
+
+func Add() {
+
+}
+
+func Remove() {
+
+}
+
+func Select() {
+
+}
+
+func Modify() {
+
+}
+
+func Count() {
+
+}
+
+func First() {
+
+}
+
+func Last() {
+
+}
