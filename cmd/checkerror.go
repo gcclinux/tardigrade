19c0bcc63bb70264ef73f7a481782fe47d6b6842
Ricardo Wagemaker <java@wagemaker.co.uk>
2023-02-01 23:06:57 +0000

    Initial push

--- cmd/checkerror.go
+++ cmd/checkerror.go
@@ -2,6 +2,7 @@ package main
 
 import "log"
 
+// CheckError receive a message and the error code, if there is a problem panic and display issues
 func CheckError(msg string, err error) {
 	if err != nil {
 		log.Println(msg)
